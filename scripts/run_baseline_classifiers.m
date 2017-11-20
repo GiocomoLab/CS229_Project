251b31384b4e7de233a7dcc3e418d15416f183a9
Mark <markplitt@gmail.com>
2017-11-20 13:42:38 -0800

    resove run baseline classifiers

--- scripts/run_baseline_classifiers.m
+++ scripts/run_baseline_classifiers.m
@@ -2,77 +2,24 @@
 
 get_data_folder;
 
-% Get all grid cells  
+% Get all grid cells
 load(fullfile(datafolder,'params.mat')); % includes cell array with filenames of grid cells
 
 % Take a random subset of non-grid cells to match prevelance
 % use fixed random seed for debugging
-rng('default'); 
+rng('default');
 
-<<<<<<< HEAD
+
 get_fnames;
-=======
+
 % get all files names
 files = dir(fullfile(datafolder,'FeatStruct_*.mat'));
 files = {files(:).name}';
 
-% find grid cells
-grid_inds = zeros(size(files));
-ngrids = length(grid_fnames);
-for g = 1:length(grid_fnames)
-    grid_inds = grid_inds + strcmp(files,grid_fnames{g});
-end
 
-% find border cells
-border_inds = zeros(size(files));
-nborders = length(border_fnames);
-for b = 1:length(border_fnames)
-    border_inds = border_inds + strcmp(files,border_fnames{b});
-end
-
-% get unique IDs
-uniqueID_grid = cell(numel(grid_fnames),1);
-for i = 1:numel(grid_fnames)
-    split = strsplit(grid_fnames{i},'_');
-    uniqueID_grid{i} = split{2};
-end
-uniqueID_border = cell(numel(border_fnames),1);
-for i = 1:numel(border_fnames)
-    split = strsplit(border_fnames{i},'_');
-    uniqueID_border{i} = split{2};
-end
-uniqueID_all = cell(numel(files),1);
-for i = 1:numel(files)
-    split = strsplit(files{i},'_');
-    uniqueID_all{i} = split{2};
-end
-
-
-% non grid cells
-nongrid_fnames = files(~endsWith(uniqueID_all,uniqueID_grid));
-
-% non border cells 
-nonborder_fnames = files(~endsWith(uniqueID_all,uniqueID_border));
-
-% get all grid | border cells
-gb_inds = logical(grid_inds+border_inds);
-gb_fnames = files(gb_inds);
-
-% non grid | border cells
-nongb_fnames = files(~endsWith(uniqueID_all,uniqueID_grid) & ~endsWith(uniqueID_all,uniqueID_border));
-
-% downsample non-functional cell types
-
-nongrid_fnames_ds = nongrid_fnames(randperm(length(nongrid_fnames),length(grid_fnames)));
-nonborder_fnames_ds = nonborder_fnames(randperm(length(nonborder_fnames),length(border_fnames)));
-nongb_fnames_ds = nongb_fnames(randperm(length(nongb_fnames),length(gb_fnames)));
-
->>>>>>> MalcolmBranch
 
 % comparisons to run
-tests = {{'grid','nongrid'}};
-% forward search order
-<<<<<<< HEAD
+tests = {{'gb', 'nongb'},{'grid','nongrid'},{'border','nonborder'}};
 forward_search_order = {{'mean_fr','fr_dft_abs'},...
     {'mean_fr','fr','fr_dft_abs'},{'mean_fr','mean_fr_ccorr','ccorr_peak','fr_dft_abs'},...
     {'mean_fr','fr','fr_dft_abs','ccorr_peak','fr_dft_abs'}};
@@ -80,17 +27,6 @@ forward_search_order = {{'mean_fr','fr_dft_abs'},...
 modelTypes = {'linear_svm','logistic', 'svm','gda'};
 % hyperparams for each calssifier
 hyperParams = {{'ridge',1e4},{'ridge',1e4},{'rbf'},{},{}};
-=======
-% forward_search_order = {{'fr'},{'fr','fr_dft_abs'}, {'fr','fr_dft_abs','ccorr_peak'},....
-%    {'fr','fr_dft_abs','ccorr_peak','mean_fr_ccorr'}};
-forward_search_order = {{'fr','fr_dft_abs'}};
-% which classifiers to run
-% modelTypes = {'linear_svm','logistic', 'svm'};
-modelTypes = {'svm'};
-% hyperparams for each classifier
-%hyperParams = {{'ridge',1e4},{'ridge',1e4},{'rbf'}};
-hyperParams = {{'rbf'}};
->>>>>>> MalcolmBranch
 results = cell(length(tests),length(forward_search_order));
 fold_inds_save = cell(length(tests),1);
 for t = 1:length(tests)
@@ -98,25 +34,25 @@ for t = 1:length(tests)
     eval(['class1_fnames = ' tests{t}{2} '_fnames_ds;']);
     m = length(class0_fnames) + length(class1_fnames);
     fold_inds = build_folds(m,m);
-    
+
     fold_inds_vector = nan(size(fold_inds));
     for i = 1:size(fold_inds,1)
         fold_inds_vector(i) = fold_inds{i};
     end
     fold_inds_save{t} = fold_inds_vector;
-    
+
     for f = 1:length(forward_search_order)
         feats = forward_search_order{f};
         [X, Y] = load_features(datafolder,{class0_fnames,class1_fnames},feats);
-        
+
         single_run_results = batch_run_cv(X,Y,feats,fold_inds,modelTypes,hyperParams);
         single_run_results.groups = tests{t};
-        
+
         results{t,f} = single_run_results;
-        
-        
+
+
     end
-    
+
 end
 
 <<<<<<< HEAD
@@ -125,33 +61,33 @@ for i =  1:length(tests)
     for j = 1:length(forward_search_order)
         cmat_train = results{i,j}.logistic.cmat_train;
         cmat_test = results{i,j}.logistic.cmat_test;
-        
+
         train_acc(i,j,1) = sum(diag(cmat_train))/sum(sum(cmat_train));
         test_acc(i,j,1) = sum(diag(cmat_test))/sum(sum(cmat_test));
-            
-        
+
+
         cmat_train = results{i,j}.linear_svm.cmat_train;
         cmat_test = results{i,j}.linear_svm.cmat_test;
-        
+
         train_acc(i,j,2) = sum(diag(cmat_train))/sum(sum(cmat_train));
         test_acc(i,j,2) = sum(diag(cmat_test))/sum(sum(cmat_test));
 
-        
+
         cmat_train = results{i,j}.svm.cmat_train;
         cmat_test = results{i,j}.svm.cmat_test;
-        
+
         train_acc(i,j,3) = sum(diag(cmat_train))/sum(sum(cmat_train));
         test_acc(i,j,3) = sum(diag(cmat_test))/sum(sum(cmat_test));
-        
+
         cmat_train = results{i,j}.gda.cmat_train;
         cmat_test = results{i,j}.gda.cmat_test;
-        
+
         train_acc(i,j,4) = sum(diag(cmat_train))/sum(sum(cmat_train));
         test_acc(i,j,4) = sum(diag(cmat_test))/sum(sum(cmat_test));
-        
+
 %         cmat_train = results{i,j}.qda.cmat_train;
 %         cmat_test = results{i,j}.qda.cmat_test;
-%         
+%
 %         train_acc(i,j,5) = sum(diag(cmat_train))/sum(sum(cmat_train));
 %         test_acc(i,j,5) = sum(diag(cmat_test))/sum(sum(cmat_test));
     end
