9fa922977c90511bed0e58182aee81c83788a401
Mark <markplitt@gmail.com>
2017-12-09 17:49:09 -0800

    replot timewarp

--- scripts/twPCA/barycenter_drift_features.py
+++ scripts/twPCA/barycenter_drift_features.py
@@ -22,17 +22,20 @@ cell_suffix = [i.split(".mat")[0] for i in cell_suffix]
 for i, fname  in enumerate(files):
     print(fname)
     matDat = sio.loadmat(fname)
-    bc = json.load(open(baseDir+"sdtw_"+cell_suffix[i]+".json"))
+    #bc = json.load(open(baseDir+"sdtw_"+cell_suffix[i]+".json"))
+    regFeatFile = "bc_ls_%s" % (cell_suffix[i])
+    #sio.savemat(baseDir+ regFeatFile+".mat",data)
+    bc = sio.loadmat(baseDir+regFeatFile+".mat")['bary_center']
     #print(bc.keys())
     frMat = matDat['frTrialMat']
-    bc = np.array(bc['firing_rate_mean_warp'])
+    #bc = np.array(bc['firing_rate_mean_warp'])
 
     # get pairwise difference between individual trials and _barycenter
     d = []
     for x in frMat:
         d.append(np.power(x - bc,2).sum())
     d = np.array(d)
-    d_norm = np.divide(d-d.min(),d.max())
+    d_norm = np.divide(d-d.min(),d.max()-d.min())
     #print(d_norm)
     x = np.linspace(0,1,d.size)
 
@@ -47,6 +50,7 @@ for i, fname  in enumerate(files):
             'p_value':p_value,'std_err':std_err,'autocorr':autocorr,
             'bary_center':bc,'d_norm':d_norm}
     # save vector, r^2 of fit and slope of fit
-    regFeatFile = "bc_ls_%s.pickle" % (cell_suffix[i])
-    with open(baseDir+regFeatFile,'wb') as handle:
-        pickle.dump(data,handle,protocol=pickle.HIGHEST_PROTOCOL)
+    regFeatFile = "bc_ls_%s" % (cell_suffix[i])
+    sio.savemat(baseDir+ regFeatFile+".mat",data)
+    #with open(baseDir+regFeatFile+".pickle",'wb') as handle:
+    #    pickle.dump(data,handle,protocol=pickle.HIGHEST_PROTOCOL)
