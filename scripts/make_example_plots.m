94716fc59356470858563b6ae8bdd28ccd476ed0
mgcampb <malcgcamp@gmail.com>
2017-12-10 12:34:01 -0800

    features for drifty bursty cells

--- scripts/make_example_plots.m
+++ scripts/make_example_plots.m
@@ -21,38 +21,93 @@ N = size(A,1);
 
 % load params
 params = readtable('../UniversalParams.xlsx');
+nbins = (params.TrackEnd-params.TrackStart)/params.BinSize;
 
-%% make firing rate and raster plots
-meanrate = nan(N,1);
-median_isi = nan(N,1);
-burstiness = nan(N,1); % = 1/(meanrate * median_isi)
-stability = nan(N,1);
-trial_stability = nan(N,1);
+% for trial-trial cross correlations
+num_lags = 5;
+
+% for field size definition
+thresh.autocorr = 0.2;
+
+%% make raster plots
+counter = 1;
+feat = table(); % feature matrix
+feat.uniqueID = cell(N*5,1);
+feat.meanrate = nan(N*5,1);
+feat.median_isi = nan(N*5,1);
+feat.burstiness = nan(N*5,1); % = 1/(meanrate * median_isi)
+feat.stability = nan(N*5,1);
+feat.stability_zscore = nan(N*5,1);
+feat.trial_stability = nan(N*5,1);
+feat.stability_ratio = nan(N*5,1); % trial stability / stability
+feat.fieldsize = nan(N*5,1);
 for i = 1:N
     fprintf('cell %d/%d\n',i,N);
     
     % load cell data
     load(sprintf('%s%s_%s_%s.mat',datafolder,A.Mouse{i},A.SessionVR{i},A.Cell{i}));
-    posx = celldata.bl.posx{1};
-    trial = celldata.bl.trial{1};
-    spike_idx = celldata.bl.spike_idx{1};
     
-    % burstiness score
-    meanrate(i) = numel(celldata.bl.spike_t{1})/max(celldata.bl.post{1});
-    median_isi(i) = median(diff(celldata.bl.spike_t{1}));
-    burstiness(i) = 1/(meanrate(i) * median_isi(i));
-    stability(i) = celldata.bl.stability(1);
-    
-    if make_figures
+    for j = 1:2:numel(celldata.bl.blocktype)
 
-        h = figure('Visible','off');
-        plot(posx(spike_idx),trial(spike_idx),'r.','MarkerSize',10);
-        xlim([params.TrackStart params.TrackEnd]);
-        ylim([0 max(trial)+1]);
-        title(sprintf('fr=%0.2f, b=%0.3f, isi=%0.2f\n, s=%0.3f, st=%0.3f, st/s=%0.3f\nfs=%0.3f',...
-            celldata.bl.bursty_score));
-        if save_figures
-            saveas(h,sprintf('plots/all_cells/%s_%s_%s.png',A.UniqueID{i},A.SessionVR{i},A.Cell{i}),'png');
+        % load data for this block
+        posx = celldata.bl.posx_filt{j};
+        trial = celldata.bl.trial_filt{j};
+        dt = celldata.bl.dt_filt{j};
+        idx = celldata.bl.spike_idx_filt{j};
+        
+        % compute firing rate
+        firing_rate = calculateSmoothedFiringRate(idx,posx,dt,params);
+        
+        % autocorrelation (for field size)
+        firing_rate_autocorr = autocorr(firing_rate,nbins-1);
+
+        % compute single trial cross-correlations
+        fr_corr = nan(max(trial)-1,num_lags*2+1);
+        for k = 2:max(trial)
+            % trial 1
+            posx1 = posx(trial==k-1);
+            dt1 = dt(trial==k-1);
+            idx1 = idx(trial(idx)==k-1);
+            idx1 = idx1 - sum(trial<k-1);
+            fr1 = calculateSmoothedFiringRate(idx1,posx1,dt1,params);
+
+            % trial 2
+            posx2 = posx(trial==k);
+            dt2 = dt(trial==k);
+            idx2 = idx(trial(idx)==k);
+            idx2 = idx2 - sum(trial<k);
+            fr2 = calculateSmoothedFiringRate(idx2,posx2,dt2,params);
+
+            % cross correlation
+            fr_corr(k-1,:) = crosscorr(fr1,fr2,num_lags);
+        end    
+        
+        % get features
+        feat.uniqueID{counter} = A.UniqueID{i};
+        feat.meanrate(counter) = numel(celldata.bl.spike_t{j})/max(celldata.bl.post{j});
+        feat.median_isi(counter) = median(diff(celldata.bl.spike_t{j}));
+        feat.burstiness(counter) = 1/(feat.meanrate(counter)*feat.median_isi(counter));
+        feat.stability(counter) = celldata.bl.stability(j);
+        feat.stability_zscore(counter) = celldata.bl.stability_zscore(j);
+        feat.trial_stability(counter) = mean(max(fr_corr,[],2));
+        feat.stability_ratio(counter) = feat.trial_stability(counter)/feat.stability(counter);
+        thresh.fieldsize(counter) = params.BinSize*(find(firing_rate_autocorr<thresh.autocorr,1)-1);
+        counter = counter+1;
+
+        if make_figures
+            h = figure('Visible','off');           
+            plot(posx(idx),trial(idx),'r.','MarkerSize',10);
+            xlim([params.TrackStart params.TrackEnd]);
+            ylim([0 max(trial)+1]);
+            if save_figures
+                saveas(h,sprintf('plots/all_cells/%s_%s_%s_%d.png',A.UniqueID{i},A.SessionVR{i},A.Cell{i},j),'png');
+            end
         end
     end
 end
+
+% filter out nans
+feat = feat(~isnan(feat.meanrate),:);
+
+% close figures
+close all
